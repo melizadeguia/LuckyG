27b6bae780dadd7000e9cb91fa4f09d680514804
Ann <cielitorepuyan@gmail.com>
2022-05-25 22:17:32 +0800

    updates
    footer header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import FrontPage from './components/FrontPage';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header/>
       <FrontPage/>
+      <Footer/>
      
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,35 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
-import logo from '../media/logo.png'
-import '../styles/Header.scss'
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
+import Logo from '../media/logo.png';
 
 const Header = () => {
   return (
     <div>
-      <Nav  activeKey="/home" className='nav'>
-        <Nav.Item>
-          <div className='img'>
-            <a href='/'><img src={logo}/></a>
-          </div>
-        </Nav.Item>
-
-        <div className="items">
-          <Nav.Item>
-            <Nav.Link href="/" className='text'>Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1" href='/reservation' className='text'>Reservations</Nav.Link>
-          </Nav.Item>
-
-          <Nav.Item>
-            <Nav.Link eventKey="link-1" href='/fleet' className='text'>Fleet</Nav.Link>
-          </Nav.Item>
-
-          <Nav.Item>
-            <Nav.Link eventKey="link-2" href='/about' className='text'>About Us</Nav.Link>
-          </Nav.Item>
-
-          <Nav.Item>
-            <Nav.Link eventKey="link-3" href='/contact' className='text'>Contact Us</Nav.Link>
-          </Nav.Item>
-        </div>
-      </Nav>
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home"><img src={Logo}/></Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#features"> About Us</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Services" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Self-Drive</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Airport Shuttle Service</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Driving School</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">With Driver</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">Reservation</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Contact Us
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
     </div>
   )
 }
